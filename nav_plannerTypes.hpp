807c24344e2d8448e4681c9fce6797e2f1329dc3
Raul Dominguez <raul.dominguez@dfki.de>
2016-07-06 13:46:32 +0200

    initial commit by rock-create-orogen

---
+++ nav_plannerTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef nav_planner_TYPES_HPP
+#define nav_planner_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace nav_planner {
+}
+
+#endif
+
